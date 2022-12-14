bb12514d049c36e64f200fe0f62e72fae54b3300
Natan Ferreira <natan.ferreira21@hotmail.com>
2022-12-14 15:04:30 -0300

    Finalizado o projeto e as correcoes.

--- src/main/java/com/facul/curso/web/domain/Funcionario.java
+++ src/main/java/com/facul/curso/web/domain/Funcionario.java
@@ -3,18 +3,10 @@ package com.facul.curso.web.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
+
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
@@ -33,7 +25,7 @@ public class Funcionario extends AbstractEntity<Long> {
 	
 	@NotNull
 	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
-	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
+	@Column(nullable = true, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
 	
 	@NotNull
